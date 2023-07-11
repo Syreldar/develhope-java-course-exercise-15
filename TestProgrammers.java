e95690595c94a3dd0d4a3c7f0b60a796a39f37c2
Syreldar <enrico.drago3@gmail.com>
2023-07-11 15:08:34 +0200

    Upload exercise

--- TestProgrammers.java
+++ TestProgrammers.java
@@ -2,5 +2,20 @@ public class TestProgrammers
 {
     public static void main(String[] args)
     {
+        Programmer programmer1 = new Programmer();
+        programmer1.name = "Rossella";
+        programmer1.age = 36;
+        programmer1.wearsGlasses = false;
+
+        programmer1.drinkCoffee();
+        programmer1.printDetails();
+
+        Programmer programmer2 = new Programmer();
+        programmer2.name = "Enrico";
+        programmer2.age = 25;
+        programmer2.wearsGlasses = true;
+
+        programmer2.printDetails();
+        programmer2.hasGlasses();
     }
 }
