7e4c439fa8f11c748b31a4586c0def2afd83b665
Syreldar <enrico.drago3@gmail.com>
2023-07-11 15:07:31 +0200

    Update Programmer.java

--- Programmer.java
+++ Programmer.java
@@ -1,3 +1,21 @@
 public class Programmer
 {
+    String name;
+    int age;
+    boolean wearsGlasses;
+
+    public void drinkCoffee()
+    {
+        System.out.println("Espresso is the secret!");
+    }
+
+    public void printDetails()
+    {
+        System.out.printf("%s is a %d-yo programmer.%n", name, age);
+    }
+
+    public void hasGlasses()
+    {
+        System.out.printf("Is %s wearing glasses? %s.%n", name, wearsGlasses ? "Yes" : "No");
+    }
 }
